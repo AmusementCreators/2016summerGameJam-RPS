862e2e5d697677a319f953ae38e12c894503917c
美由樹 根本 <3yukiyu3@gmail.com>
2016-08-03 19:21:38 +0900

    copyrightを修正

--- RPS/CopyRightScene.cs
+++ RPS/CopyRightScene.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace RPS
 {
-    class CopyRightScene :asd.Scene
+    class CopyRightScene : asd.Scene
     {
+        asd.Layer2D layer = null;
 
         private void Copyright()
         {
             // フォントを生成する。
-             var font = asd.Engine.Graphics.CreateDynamicFont("",32, new asd.Color(255, 255, 255),0, new asd.Color(0, 0, 0));
+            var font = asd.Engine.Graphics.CreateDynamicFont("", 24, new asd.Color(255, 255, 255), 0, new asd.Color(0, 0, 0));
 
             // 文字描画オブジェクトを生成する。
             var obj = new asd.TextObject2D();
@@ -21,14 +22,17 @@ namespace RPS
             obj.Font = font;
 
             // 描画位置を指定する。
-            obj.Position = new asd.Vector2DF(200, 200);
+            obj.Position = new asd.Vector2DF(200, 120);
 
             // 描画する文字列を指定する。
-            obj.Text = "spotlight image \n Freepik(http://jp.freepik.com/free-vector/ベクター画像_780623.htm) \n\n background image \n Design by MysticEmma(http://zooll.com/goodies-pastel-triangle-patterns/) \n\n animation \n Live2D(http://www.live2d.com/ja/) \n\n\n PUSH　SPACE-key TO BACK";
+            obj.Text = " --spotlight image-- \n Freepik(http://jp.freepik.com/free-vector/ベクター画像_780623.htm) \n \n --background image-- \n Design by MysticEmma(http://zooll.com/goodies-pastel-triangle-patterns/) \n \n --animation-- \n Live2D(http://www.live2d.com/ja/) \n \n This game is created by wisteria, kengpeng. \n © 2016 AmusementCreators \n \n \n PUSH　SPACE-key TO BACK";
+
+            layer = new asd.Layer2D();
+            AddLayer(layer);
 
             // 文字描画オブジェクトのインスタンスをエンジンへ追加する。
-            asd.Engine.AddObject2D(obj);
-    }
+            layer.AddObject(obj);
+        }
 
         public CopyRightScene()
         {
@@ -41,5 +45,5 @@ namespace RPS
                 asd.Engine.ChangeSceneWithTransition(new TitleScene(), new asd.TransitionFade(0.5f, 0.5f));
             }
         }
-}
+    }
 }
